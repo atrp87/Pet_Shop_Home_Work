7a408ea4fbab478bd9f809ed8c0897a3c785a014
atrp87 <drewpeattie@hotmail.com>
2020-02-22 17:45:37 +0000

    added home_work

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,14 +2,30 @@ def pet_shop_name(name)
   return name[:name]
 end
 
-
-total = 0
-
 def total_cash(sum)
   return sum[:admin][:total_cash]
 end
 
+def add_or_remove_cash(shop, cash)
+    return shop[:admin][:total_cash] += cash
+end
 
+def add_or_remove_cash(shop, cash)
+  shop[:admin][:total_cash] -= cash
+end
 
-def add_or_remove_cash(cash, total)
-  
+def stock_count(count)
+  return count[:pets].length
+end
+
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+end
+
+def customer_cash(cash)
+  return cash[:cash]
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push() new_pet
+end
